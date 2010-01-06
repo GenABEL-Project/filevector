d298577c62ee7f6191ee623b737cd392ffb08b01
Stepan Yakovenko <stiv@dote.ru>
2010-01-06 08:50:21 +0000

    code refactor
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/filevector@107 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/FileModificationTest.cpp
+++ tests/FileModificationTest.cpp
@@ -7,18 +7,19 @@
 
 using namespace std;
 
-
-
 string FileModificationTest::get_file_name_to_write()
 {
-    return  TestUtil::get_base_dir() + string("/../tests/data/2write/modify_me.fvf");
+    return TestUtil::get_base_dir() + string("/../tests/data/2write/modify_me.fvf");
 }
-
 
 int main( int argc, char **argv)
 {
+    string file_name = get_file_name_to_write();
+
     TestUtil::detect_base_dir(string(argv[0]));
-    cout << "base_dir :" << TestUtil::get_base_dir() << "\n";
+    filevector<float> indata1( ifname1, cachesize );
+
+    cout << "base_dir:" << TestUtil::get_base_dir() << endl;
 
     CppUnit::TextUi::TestRunner runner;
     runner.addTest( FileModificationTest::suite() );
