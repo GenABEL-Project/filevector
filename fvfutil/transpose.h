d95373531e69808d9c646fba2d1ea1dd895a95db
Andrey Chernyh <chernyh@dote.ru>
2010-01-25 08:09:37 +0000

    first working buggish version
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/filevector@214 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- fvfutil/transpose.h
+++ fvfutil/transpose.h
@@ -8,4 +8,14 @@ void write_var_obs_names(filevector * src_fv, filevector * dest_fv);
 
 void copy_data(string src_data_file_name,string dest_data_file_name, unsigned long int src_nvars,
 unsigned long int src_nobss, unsigned int data_size);
+
+void read_part(ifstream * src_stream, char * data_part, unsigned long int obs_start , unsigned long int obs_length,
+unsigned long int var_start, unsigned long int var_length , unsigned int  data_size);
+
+void write_part(ofstream * src_stream, char * data_part_transposed, unsigned long int obs_start , unsigned long int obs_length,
+unsigned long int var_start, unsigned long int var_length , unsigned int  data_size);
+
+void transpose_part(char * data_part, char * data_part_transposed,
+unsigned long int obs_length,unsigned long int var_length, unsigned int data_size );
+
 };
