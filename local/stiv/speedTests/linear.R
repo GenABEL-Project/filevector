3170d69d66bc4780e7905e862ecad978560fa355
Stepan Yakovenko <stiv@dote.ru>
2010-07-20 12:53:10 +0000

    text2fvf speedup
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/genabel/pkg/filevector@546 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- local/stiv/speedTests/linear.R
+++ local/stiv/speedTests/linear.R
@@ -1,18 +1,15 @@
 library(DatABEL);
 
 f <- function(n, type){
-d=100;
-f=make_empty_fvf("name",n,n,type="DOUBLE",cachesize=10);
-system.time(for(i in 1:(n/d)){s=s+databel2matrix(f,,i);})*d
-rm(f);gc();
-unlink("name*");
+d=20;
+s=c(1:n);
+f=make_empty_fvf(paste("name",toString(n),type),n,n,type,cachesize=1);
+print(system.time(for(i in 1:(n/d)){s=s+databel2matrix(f,,i);})*d)
+#rm(f);gc();
+#unlink("name*");
 }
 
-
-f(3000,"CHAR")
-f(3000,"SHORT_INT");
-f(3000,"INT");
-f(3000,"DOUBLE");
+unlink("name*");
 f(5000,"CHAR")
 f(5000,"SHORT_INT");
 f(5000,"INT");
@@ -21,3 +18,13 @@ f(10000,"CHAR")
 f(10000,"SHORT_INT");
 f(10000,"INT");
 f(10000,"DOUBLE");
+f(20000,"CHAR")
+f(20000,"SHORT_INT");
+f(20000,"INT");
+f(20000,"DOUBLE");
+f(30000,"CHAR")
+f(30000,"SHORT_INT");
+f(30000,"INT");
+f(30000,"DOUBLE");
+
+
